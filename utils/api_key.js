84308a9051833b06d4f65df73b3411af81979876
Liberontissauri <jp.liberon@gmail.com>
2021-07-29 11:40:07 +0100

    added keyExists function

---
+++ utils/api_key.js
@@ -0,0 +1,23 @@
+const validator = require("validator")
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    searchPath: ['knex', 'public'],
+})
+
+async function keyExists(key_id) {
+    if(!validator.isUUID(key_id)) return false
+
+    stored_key = await knex("API_KEYS").where("key_id", key_id)
+    if (stored_key.length == 0) return false
+
+    return stored_key[0]
+}
+
+module.exports.keyExists = keyExists
