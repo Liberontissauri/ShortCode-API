5891d846a44d6c017bedf97873eb85efe6b5a3fe
Liberontissauri <jp.liberon@gmail.com>
2021-07-29 15:33:43 +0100

    added /codes Route

--- routes/code.js
+++ routes/code.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const router = express.Router()
 
-router.use("/apikeys", require("./api_key"))
-router.use("/codes", require("./code"))
+
 
 module.exports = router
