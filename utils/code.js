60b2ad75c34f7662a7257c6515b9c4e380fc310e
Liberontissauri <jp.liberon@gmail.com>
2021-07-30 12:37:23 +0100

    added code utils

---
+++ utils/code.js
@@ -0,0 +1,23 @@
+const validator = require("validator")
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    searchPath: ['knex', 'public'],
+})
+
+async function codeExists(code_id) {
+    if(!validator.isUUID(code_id)) return false
+
+    stored_code = await knex("CODES").where("code_id", code_id)
+    if (stored_code.length == 0) return false
+
+    return stored_code[0]
+}
+
+module.exports.codeExists = codeExists
--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,5 @@
 const api_key_utils = require("./api_key")
+const code_utils = require("./code")
 
 module.exports.api_key = api_key_utils
+module.exports.code = code_utils
